c80f9c7c87ffec9318c81fa31072a4d721c1e3dd
Александр Осипов <alexosipov200695@gmail.com>
2021-12-22 12:25:11 +0100

    Thirdcommit of the lesson 4, edited test_main_page file

---
+++ test_main_page.py
@@ -0,0 +1,7 @@
+from .pages.main_page import MainPage
+
+def test_guest_can_go_to_login_page(browser):
+    url = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, url)
+    page.open()
+    page.go_to_login_page()
