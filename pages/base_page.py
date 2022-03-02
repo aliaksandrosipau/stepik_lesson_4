803e6b33d68a03c9171580f025de95c258e69c45
Александр Осипов <alexosipov200695@gmail.com>
2022-03-02 10:39:45 +0100

    Stepik lesson 4. Inheritance practice tasks.

--- pages/base_page.py
+++ pages/base_page.py
@@ -5,6 +5,7 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 from selenium.common.exceptions import TimeoutException
 import math
+from .locators import BasePageLocators
 
 class BasePage:
 
@@ -51,3 +52,25 @@ class BasePage:
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
+
+    def go_to_login_page(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert "login" in self.browser.current_url, "It's not a login url"
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_FORM), "Login form is not presented"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*BasePageLocators.REGISTER_FORM), "Register form is not presented"
--- pages/locators.py
+++ pages/locators.py
@@ -2,11 +2,12 @@ from selenium.webdriver.common.by import By
 
 
 class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    pass
 
 class LoginPageLocators():
-    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
-    REGISTER_FORM = (By.CSS_SELECTOR, "register_form")
+    # LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    # REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    pass
 
 class ProductPageLocators():
     ADD_TO_BASKET_BUTTON = (By.XPATH, '//button[@class="btn btn-lg btn-primary btn-add-to-basket"]')
@@ -15,3 +16,9 @@ class ProductPageLocators():
     PRICE_IN_BOOK_DETAILS = (By.XPATH, '//p[@class="price_color"]')
     PRICE_IN_MESSAGE = (By.XPATH, '//div[@class="alertinner "]/p/strong')
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, '.alert-success')
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
